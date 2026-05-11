ebb7da5efba6a63cd3bde4087b4ad2043f92c1d4
Brantlab <justin@brantlab.com>
2026-05-10 20:24:17 -0400

    Added archive for access.

---
+++ static/W8FY Archive/photos/2013/Hamfest2013/album/res/search.js
@@ -0,0 +1,14 @@
+Search={
+	sep:'-!-',
+	makeSlides:false,
+	folderThumb:'folderthumb.jpg',
+	indexName:'index.html',
+	thumbs:'thumbs',
+	slides:'slides',
+	ext:'html',
+	urlEncode:false,
+	text:{title:'Search...',headln:'Search results for',notFound:'Not found',close:'Close'}
+};
+Search.data=[
+	[ '', [ 'index.html-!-Hamfest2013-!--!-','20130721_070830 (Medium).jpg-!--!-20130721 070830 (Medium)  Outside Sales-!--!--!-0','20130721_070836 (Medium).jpg-!--!-20130721 070836 (Medium)  Outside Sales-!--!--!-0','20130721_075026 (Medium).jpg-!--!-20130721 075026 (Medium)  Outside working hard-!--!--!-0','20130721_070841 (Medium).jpg-!--!-20130721 070841 (Medium)  Outside Sales-!--!--!-0','20130721_071014 (Medium).jpg-!--!-20130721 071014 (Medium)  Ouside Sales-!--!--!-0' ] ]
+];
---
+++ static/W8FY Archive/photos/2013/Tractor Tour 2013/album/res/search.js
@@ -0,0 +1,14 @@
+Search={
+	sep:'-!-',
+	makeSlides:false,
+	folderThumb:'folderthumb.jpg',
+	indexName:'index.html',
+	thumbs:'thumbs',
+	slides:'slides',
+	ext:'html',
+	urlEncode:false,
+	text:{title:'Search...',headln:'Search results for',notFound:'Not found',close:'Close'}
+};
+Search.data=[
+	[ '', [ 'index.html-!-Tractor Tour 2013-!--!-','20130803_090654 (Medium).jpg-!--!-20130803 090654 (Medium)  Ralph-!--!--!-0','20130803_090709 (Medium).jpg-!--!-20130803 090709 (Medium)  T and he Start of Tour Thanks to Mike KB8UXX, Ralph WB8YIH, Jerry KB8MAF and Dave \"Ziggy\" KA8ZGE-!--!--!-0','20130803_090704 (Medium).jpg-!--!-20130803 090704 (Medium)  All Lined up and ready to go.-!--!--!-0','20130803_115126 (Medium).jpg-!--!-20130803 115126 (Medium)  Our stop at Mendon Harley Dealer-!--!--!-0','20130803_115122 (Medium).jpg-!--!-20130803 115122 (Medium)  Stopped at Mendon for Lunch break-!--!--!-0','20130803_115109 (Medium).jpg-!--!-20130803 115109 (Medium)  Nice restored Tractor-!--!--!-0','20130803_115151 (Medium).jpg-!--!-20130803 115151 (Medium)  Getting ready to head back to Van Wert-!--!--!-0' ] ]
+];
