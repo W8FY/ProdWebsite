b50f2e10697ba280bf2bc1f6c495609628087a29
freelsdr <freelsdr@gmail.com>
2026-08-27 14:17:36 -0400

    Point net check-in to new Apps Script backend

--- net-checkin/js/config.js
+++ net-checkin/js/config.js
@@ -2,6 +2,6 @@
   "use strict";
 
   window.W8FY_GOOGLE_APPS_SCRIPT_CONFIG = window.W8FY_GOOGLE_APPS_SCRIPT_CONFIG || {
-    url: "https://script.google.com/macros/s/AKfycbz8-yf9TFHV75Z2V8uAEDWYJLh2o9so5e9LtvNtA12GceqxeGcSVscMpM1G1Lcv75JF/exec"
+    url: "https://script.google.com/macros/s/AKfycbx8vYN0_dhxsehB1kMzFO6N5NFfObC9fui12FOovUYcPRzOEj9qyeiBMruSk2vNNjlx/exec"
   };
 }());
